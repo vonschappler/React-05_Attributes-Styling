fae1e5fab897426e310a7ad87a02e054c50375d4
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 14:31:22 +0000

    Correct image challenge

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ ReactDOM.render(
   <div>
     <h1 className='heading'>My Favourite Foods</h1>
     <div>
-      <img src='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
-      <img src='https://i.pinimg.com/736x/44/29/f0/4429f02128255f000ff0f11e03fc2cb2.jpg'/>
-      <img src='https://picsum.photos/id/237/536/354'/>
+      <img className='any-img' src='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
+      <img className='any-img' src='https://i.pinimg.com/736x/44/29/f0/4429f02128255f000ff0f11e03fc2cb2.jpg'/>
+      <img className='any-img' src='https://picsum.photos/id/237/536/354'/>
     </div>
   </div>,
   document.getElementById("root")
