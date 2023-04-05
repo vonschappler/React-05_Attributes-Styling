f2b6b731475e65a1da91f3d6131923ddea24b917
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 14:29:08 +0000

    Complete challenge with images

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1 className='heading'>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <div>
+      <img src='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
+      <img src='https://i.pinimg.com/736x/44/29/f0/4429f02128255f000ff0f11e03fc2cb2.jpg'/>
+      <img src='https://picsum.photos/id/237/536/354'/>
+    </div>
   </div>,
   document.getElementById("root")
 );
